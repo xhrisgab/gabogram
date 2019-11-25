0ce5d31dd45032c5d866e54b732271c34b59fc17
Christian Cuevas <xhris.gab@gmail.com>
2019-11-25 12:23:29 -0400

    Django - Profile extention to User admin, users app

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,10 @@
 """User admin Classes."""
 #Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 #Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 # Register your models here.
@@ -18,7 +20,31 @@ class ProfileAdmin(admin.ModelAdmin):
 
     fieldsets = (
         ('Profile',{
-            'fields': ('user', 'picture'),
+            'fields': (('user', 'picture'),),
         }),
+        ('Extra Info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography'),
+            )
 
+        }),
+        ('Metadata',{
+            'fields':(('created', 'modified'),),
+        })
     )
+    readonly_fields = ('created', 'modified')
+    
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users."""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+    inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
