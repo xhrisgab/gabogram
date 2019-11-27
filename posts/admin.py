2853517e3a2c65183cab7764c09f8d8dd414d8a2
Christian Cuevas <xhris.gab@gmail.com>
2019-11-27 12:27:37 -0400

    it was added class PostAdmin and was included on admin manager

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
+"""Posts admin classes."""
+#Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
-# Register your models here.
+#Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Posts Admin Model."""
+
+    list_display = ('pk', 'user', 'photo')
+    list_display_links = ('pk', 'user')
+    list_editable = ('photo',)
+    list_filter = ('created', 'modified')
